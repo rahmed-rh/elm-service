6adf8035f55cd9bb1f11663d282c4122a883cc6c
Raif Ahmed <rahmed@redhat.com>
2018-07-04 10:09:02 +0300

    Initial project

---
+++ src/main/java/sa/elm/webservice/CamelRouter.java
@@ -0,0 +1,63 @@
+package sa.elm.webservice;
+
+import javax.ws.rs.core.MediaType;
+
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.model.rest.RestBindingMode;
+import org.springframework.stereotype.Component;
+
+import sa.elm.webservice.bean.LDAPRequest;
+import sa.elm.webservice.bean.ResponseData;
+import sa.elm.webservice.service.LDAPAuthenticationService;
+
+/**
+ * A simple Camel REST DSL route that implement the greetings service.
+ * 
+ */
+@Component
+public class CamelRouter extends RouteBuilder {
+
+
+    @Override
+    public void configure() throws Exception {
+
+    	// @formatter:off
+        restConfiguration()
+                .apiContextPath("/api-doc")
+                .apiProperty("api.title", "Greeting REST API")
+                .apiProperty("api.version", "1.0")
+                .apiProperty("cors", "true")
+                .apiProperty("base.path", "camel/")
+                .apiProperty("api.path", "/")
+                .apiProperty("host", "")
+//                .apiProperty("schemes", "")
+                .apiContextRouteId("doc-api")
+                .enableCORS(true)
+            .component("servlet")
+            .bindingMode(RestBindingMode.json);
+       
+        rest("/user/")
+        	//swagger
+        	.description("Sample API")
+        		.produces(MediaType.APPLICATION_JSON).consumes(MediaType.APPLICATION_JSON)
+        		.skipBindingOnErrorCode(false) //Enable json marshalling for body in case of errors
+            .post("/auth")
+            //swagger
+            .description("Authenticate an LDAP user")
+            	.type(LDAPRequest.class).description("LDAP Request")
+            	.responseMessage().code(200).responseModel(ResponseData.class).endResponseMessage() //OK
+            	//.responseMessage().code(500).responseModel(ApiResponse.class).endResponseMessage() //Not-OK
+            //route
+                .route().routeId("auth-api")
+                .tracing().log("recieved username : ${body.username}")
+                .log("value of conigmap is ${properties:ldap.server.ip}")
+                .to("direct:validateAuth")
+        .endRest();
+
+        from("direct:validateAuth").description("Validate Auth by username & password")
+            .streamCaching()
+            .bean(LDAPAuthenticationService.class,"validateAuthDummy(${body.username},${body.password})");  
+        // @formatter:on
+    }
+
+}
